ad6b2c995fc201ee940645129a8814fbf7cbbc7c
Pavankumar Mudigonda <pavankumar.mudigonda309@gmail.com>
2024-07-17 10:43:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,74 @@
+'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+let enteredNumbers=[];
+const values = document.querySelector(".guesses")
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+
+  // When there is no input
+  if (!guess) {
+    // document.querySelector('.message').textContent = '⛔️ No number!';
+    displayMessage('⛔️ No number!');    
+  } 
+  else if(guess>20){
+    displayMessage("Choose between 1 & 20")
+  }
+  
+  
+  // When player wins
+  else if (guess === secretNumber) {
+    displayMessage(`🎉 Correct Number! 
+      click again to continue`);
+    document.querySelector('.number').textContent = secretNumber;
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    enteredNumbers.push(guess);
+    values.textContent= enteredNumbers
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+
+    // When guess is wrong
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? '📈 Too high!' : '📉 Too low!');
+      score--;
+      document.querySelector('.score').textContent = score;
+      enteredNumbers.push(guess)
+      values.textContent= enteredNumbers
+    } else {
+      displayMessage('💥 You lost the game!');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  displayMessage('Start guessing...');
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  enteredNumbers = []
+  values.textContent= enteredNumbers
+});
+
+
